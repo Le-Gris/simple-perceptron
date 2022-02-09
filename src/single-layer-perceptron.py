d90bdced5e4dc8286588685b7f2aed3fd0335fc9
alephG <solimlegris@gmail.com>
2022-02-09 17:25:46 -0500

    perceptron algo

---
+++ src/single-layer-perceptron.py
@@ -0,0 +1,29 @@
+import numpy as np
+import argparse
+
+
+class Perceptron:
+    
+    def __init__(self):
+        
+        # Number of units
+        n = 10
+
+        # Initialize weights
+        W = np.random.uniform((n, n))
+
+        # Initialize state
+        state = np.zeros(n)
+
+        # 
+
+    def activation(self):
+        pass
+
+    def update(self):
+        pass
+
+    def 
+
+
+
